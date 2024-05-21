747f23200ed3c3b4bc29f2e02433cf14c92721c0
Jeff Casavant <jeff.casavant@gmail.com>
2024-05-21 13:16:08 -0400

    feat: Make port configurable (#1)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 
+CAPI_PORT=${CAPI_PORT:-8080}
+
 set -e
 
 cd competition_api && poetry run alembic upgrade head && cd -
 
-poetry run uvicorn competition_api.main:app --host 0.0.0.0 --port 80
+poetry run uvicorn competition_api.main:app --host 0.0.0.0 --port "${CAPI_PORT}"
