ee6ccf3497365330daeeca6a89b5058c8cbe8b8d
Jeff Casavant <jeff.casavant@gmail.com>
2024-07-10 15:05:26 -0400

    Add SSL support, fix port get call (#90)

--- competition_api/tasks/pool.py
+++ competition_api/tasks/pool.py
@@ -4,13 +4,15 @@ from vyper import v
 
 v.set_default("redis.host", "127.0.0.1")
 v.set_default("redis.port", 6379)
+v.set_default("redis.ssl", False)
 
 
 def build_redis_settings() -> RedisSettings:
     return RedisSettings(
         host=v.get("redis.host"),
-        port=v.get("redis.port"),
+        port=v.get_int("redis.port"),
         password=v.get("redis.password"),
+        ssl=v.get_bool("redis.ssl"),
     )
 
 
